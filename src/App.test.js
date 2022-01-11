c3c0384c016709ed2a92f184561ef96dd8b1d907
et-nat1995 <et.natanov@gmail.com>
2022-01-11 09:55:20 -0700

    Add Button with text of Increment

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Counter Testing', () => {
   test('Render the title of the counter.', () => {
-    render(<App />);
-    const linkElement = screen.getByText('This is counter app.');
-    expect(linkElement).toBeInTheDocument();
+    const wrapper = shallow(<App/> )
+    expect(wrapper.find('h1').text()).toContain('This is counter app.');
+  });
+
+  test('Render A Button With Text of Increment', () => {
+    const wrapper = shallow(<App/> )
+    expect(wrapper.find('#increment-btn').text()).toBe('Increment')
   });
 })
